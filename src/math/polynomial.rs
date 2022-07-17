72a304c5d90839e7de46b6c4c4743c897d45ff34
Valpaq <viktor.paramonov2000@gmail.com>
2022-07-18 01:21:17 +0300

    full polynomial

--- src/math/polynomial.rs
+++ src/math/polynomial.rs
@@ -20,7 +20,7 @@ pub fn leading_coefficient(polynom: &[FieldElement]) -> FieldElement{
     return polynom[degree_of(polynom) as usize];
 }
 
-pub fn add(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
+pub fn polynom_add(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
 {
     let result_len = core::cmp::max(a.len(), b.len());
     let mut result = Vec::with_capacity(result_len);
@@ -32,7 +32,7 @@ pub fn add(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
     result
 }
 
-pub fn sub(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
+pub fn polynom_sub(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
 {
     let result_len = core::cmp::max(a.len(), b.len());
     let mut result = Vec::with_capacity(result_len);
@@ -44,7 +44,7 @@ pub fn sub(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
     result
 }
 
-pub fn mul(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
+pub fn polynom_mul(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
 {
     let result_len = a.len() + b.len() - 1;
     let mut result = FieldElement::zeroed_vector(result_len);
@@ -57,7 +57,7 @@ pub fn mul(a: &[FieldElement], b: &[FieldElement]) -> Vec<FieldElement>
     result
 }
 
-pub fn divide(a: &[FieldElement], b: &[FieldElement]) -> Option<(Vec<FieldElement>, Vec<FieldElement>)>
+pub fn polynom_division(a: &[FieldElement], b: &[FieldElement]) -> Option<(Vec<FieldElement>, Vec<FieldElement>)>
 {
     let mut apos = degree_of(&a);
     let mut a = a.to_vec();
@@ -68,7 +68,7 @@ pub fn divide(a: &[FieldElement], b: &[FieldElement]) -> Option<(Vec<FieldElemen
     }
 
     let mut result = FieldElement::zeroed_vector(apos - bpos + 1);
-    for i in (0..result.len()) {
+    for i in 0..result.len() {
         if apos < bpos {
             break;
         }
@@ -76,11 +76,113 @@ pub fn divide(a: &[FieldElement], b: &[FieldElement]) -> Option<(Vec<FieldElemen
         let shift = degree_of(&a) - degree_of(&b);
         let mut subtractee = FieldElement::zeroed_vector(shift);
         subtractee.push(coefficient);
-        subtractee = mul(&subtractee, &b);
+        subtractee = polynom_mul(&subtractee, &b);
         result[shift] = coefficient;
-        a = sub(&a, &subtractee);
+        a = polynom_sub(&a, &subtractee);
         apos = degree_of(&a);
     }
 
     Some((result, a))
+}
+
+pub fn polynom_div(a: &[FieldElement], b: &[FieldElement]) 
+    -> Option<Vec<FieldElement>>
+{
+    let (quo, rem) = match polynom_division(a, b){
+        Some((quo, rem)) => (quo, rem),
+        None => {return None;}
+    };
+    assert!(rem.len() == 0, "remainder is not zero");
+    return Some(quo);
+}
+
+pub fn polynom_module(a: &[FieldElement], b: &[FieldElement]) 
+    -> Option<Vec<FieldElement>>
+{
+    let (quo, rem) = match polynom_division(a, b){
+        Some((quo, rem)) => (quo, rem),
+        None => {return None;}
+    };
+    return Some(rem);
+}
+
+pub fn polynom_xor(a: &[FieldElement], exponent: u128) -> Vec<FieldElement> {
+    if is_zero(a){
+        return Vec::new();
+    }
+    if exponent == 0 {
+        return vec![FieldElement::ONE];
+    }
+    let mut acc = vec![FieldElement::ONE];
+    for i in 128..0 {
+        acc = polynom_mul(&acc, &acc);
+        if (1 << i) & exponent != 0 {
+            acc = polynom_mul(&acc, a);
+        }
+    }
+    acc
+}
+
+pub fn evaluate(a: &[FieldElement], point: FieldElement) -> FieldElement {
+    let mut xi = FieldElement::ONE;
+    let mut value = FieldElement::ZERO;
+    for c in a.iter(){
+        value = value + *c * xi;
+        xi = xi * point;
+    }
+    value
+}
+
+pub fn evaluate_domain(a: &[FieldElement], 
+        domain: &[FieldElement]) -> Vec<FieldElement> {
+    
+    let mut result: Vec<FieldElement> = Vec::new();
+    for i in domain {
+        result.push(evaluate(a, *i));
+    }
+    result
+}
+
+pub fn interpolate_domain(domain: &[FieldElement], 
+        values: &[FieldElement]) -> Vec<FieldElement> {
+    
+    let domain_len = domain.len();
+    assert!(domain_len == values.len(), "number of elements in domain does not match");
+    assert!(domain_len > 0, "cannot interpolate between zero points");
+    let x = vec![FieldElement::ZERO, FieldElement::ONE];
+    let mut res = vec![];
+    for i in 0..domain_len {
+        let mut prod = vec![values[i]];
+        for j in 0..domain_len {
+            if j == i{
+                continue;
+            }
+            let first = polynom_sub(&x, &[domain[j]]);
+            let second = vec![(domain[i] - domain[j]).inverse()];
+            prod = polynom_mul(&prod, &(polynom_mul(&first, &second)));
+        }
+        res = polynom_add(&res, &prod);
+    }
+    res
+}
+
+pub fn zerofier_domain(domain: &[FieldElement]) -> Vec<FieldElement> {
+    let x = vec![FieldElement::ZERO, FieldElement::ONE];
+    let mut acc = vec![FieldElement::ONE];
+    for d in domain{
+        acc = polynom_mul(&acc, &polynom_sub(&x, &[*d]));
+    }
+    acc
+}
+
+pub fn scale(x: &[FieldElement], factor: FieldElement) -> Vec<FieldElement> {
+    let mut polynom: Vec<FieldElement> = Vec::new();
+    for i in 0..x.len() {
+        polynom.push((factor ^ (i as u128)) * x[i]);
+    }
+    polynom
+}
+
+pub fn test_colinearity(domain: &[FieldElement], values: &[FieldElement]) -> bool {
+    degree_of(&interpolate_domain(domain, values)) == 1
 }
