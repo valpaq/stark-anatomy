0b6c5fce2e205504c56834a7d15bcb8ae6ca6acb
Valpaq <viktor.paramonov2000@gmail.com>
2022-07-24 20:13:43 +0300

    small adds

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -4,15 +4,13 @@ pub struct Merkle {}
 
 impl Merkle {
 
-    pub fn commit(leafs: &[u8]) -> [u8; 32] {
+    fn commit_(leafs: &[u8]) -> [u8; 32] {
         assert!(leafs.len() & (leafs.len() - 1) == 0, "length must be power of two");
         if leafs.len() == 1{
             return leafs.try_into().unwrap();
         }
         else {
-            // return Blake2s256::digest(Self::commit(&leafs[0..leafs.len()/2])
-            //     .append(Self::commit(&leafs[leafs.len()/2..])));
-            return Blake2s256::digest(&Self::concat(&Self::commit(&leafs[0..leafs.len()/2]), &Self::commit(&leafs[leafs.len()/2..]))).into()
+            return Blake2s256::digest(&Self::concat(&Self::commit_(&leafs[0..leafs.len()/2]), &Self::commit_(&leafs[leafs.len()/2..]))).into()
         }
     }
 
@@ -23,23 +21,23 @@ impl Merkle {
         concatenated
     }
 
-    pub fn open(index: usize, leafs: &[u8]) -> Vec<u8> {
+    fn open_(index: usize, leafs: &[u8]) -> Vec<u8> {
         assert!(leafs.len() & (leafs.len() - 1) == 0, "must be power of two");
         assert!((0 <= index) && (index < leafs.len()), "cannot open invalid index");
         if leafs.len() == 2 {
             return vec![leafs[1-index]];
         }
         if index < leafs.len() / 2 {
-            let mut recur_vec = Self::open(index, &leafs[..leafs.len()/2]);
-            recur_vec.extend_from_slice(&Self::commit(&leafs[leafs.len()/2..]));
+            let mut recur_vec = Self::open_(index, &leafs[..leafs.len()/2]);
+            recur_vec.extend_from_slice(&Self::commit_(&leafs[leafs.len()/2..]));
             return recur_vec;
         }
-        let mut recur_vec = Self::open(index - leafs.len()/2, &leafs[leafs.len()/2..]);
-        recur_vec.extend_from_slice(&Self::commit(&leafs[..leafs.len()/2]));
+        let mut recur_vec = Self::open_(index - leafs.len()/2, &leafs[leafs.len()/2..]);
+        recur_vec.extend_from_slice(&Self::commit_(&leafs[..leafs.len()/2]));
         return recur_vec;
     }
 
-    pub fn verify(root: [u8; 32], index: usize, path: &[&[u8]], leaf: [u8; 32]) -> bool {
+    fn verify_(root: [u8; 32], index: usize, path: &[&[u8]], leaf: [u8; 32]) -> bool {
         assert!((0 <= index) && (index < (1 << path.len())), "cannot verify invalid index");
         if path.len() == 1 {
             if index == 0 {
@@ -51,11 +49,15 @@ impl Merkle {
         }
         else {
             if index % 2 == 0 {
-                return Self::verify(root, index >> 1, &path[1..],  Blake2s256::digest(Self::concat(&leaf, path[0])).into());
+                return Self::verify_(root, index >> 1, &path[1..],  Blake2s256::digest(Self::concat(&leaf, path[0])).into());
             }
             else {
-                return Self::verify(root, index >> 1, &path[1..],  Blake2s256::digest(Self::concat(path[0], &leaf)).into());
+                return Self::verify_(root, index >> 1, &path[1..],  Blake2s256::digest(Self::concat(path[0], &leaf)).into());
             }
         }
     }
+
+    // pub fn commit(data_array: &[[u8;32]]) -> [u8; 32] {
+    //     return Self::commit_(data_array.iter().map(|x| Blake2s256::digest(x).into()).collect::<Vec<u8>>().try_into().unwrap())
+    // }
 }
--- src/proof_stream.rs
+++ src/proof_stream.rs
@@ -3,7 +3,7 @@ use std::slice;
 use crate::math::errors::DeserializationError;
 
 pub struct ProofStream{
-    pub objects: Vec<u128>,
+    pub objects: Vec<Vec<u8>>,
     pub read_index: usize
 }
 
@@ -11,23 +11,23 @@ impl ProofStream {
 
     pub fn new() -> Self {
         ProofStream {
-            objects: Vec::<u128>::new(),
+            objects: Vec::<Vec<u8>>::new(),
             read_index: 0
         }
     }
 
-    pub fn push(mut self, obj: u128) {
+    pub fn push(mut self, obj: Vec<u8>) {
         self.objects.push(obj);
     }
 
-    pub fn pull(mut self) -> u128 {
+    pub fn pull(mut self) -> Vec<u8> {
         assert!(self.read_index < self.objects.len(), "ProofStream: queue empty");
-        let obj = self.objects[self.read_index];
+        let obj = &self.objects[self.read_index];
         self.read_index += 1;
-        return obj;
+        return obj.to_vec();
     }
 
-    fn vf_to_u8(v: &[u128]) -> &[u8] {
+    fn vf_to_u8(v: &[Vec<u8>]) -> &[u8] {
         unsafe { std::slice::from_raw_parts(v.as_ptr() as *const u8, v.len() * 4) }
     }
 
